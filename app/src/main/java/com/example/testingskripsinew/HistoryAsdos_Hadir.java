6115055d4be8f13ef7d86f9b5a06db8b947cdab1
joe_fachrizal <joefachrizal@gmail.com>
2021-01-08 19:58:36 +0700

    test commit

---
+++ app/src/main/java/com/example/testingskripsinew/HistoryAsdos_Hadir.java
@@ -0,0 +1,14 @@
+package com.example.testingskripsinew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HistoryAsdos_Hadir extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.history_asdos);
+    }
+}
