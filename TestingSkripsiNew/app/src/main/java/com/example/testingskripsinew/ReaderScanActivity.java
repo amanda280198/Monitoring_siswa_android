a2bbd8da2360feac563a7b46bab4691fb16a4650
amanda280198 <54200489+amanda280198@users.noreply.github.com>
2021-01-08 16:32:08 +0700

    Add files via upload

---
+++ TestingSkripsiNew/app/src/main/java/com/example/testingskripsinew/ReaderScanActivity.java
@@ -0,0 +1,14 @@
+package com.example.testingskripsinew;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReaderScanActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_reader_scan);
+        }
+    }
