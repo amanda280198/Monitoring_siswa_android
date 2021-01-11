22b4eb6133da7c65f527a86fa8720f0a486b57da
joe_fachrizal <joefachrizal@gmail.com>
2021-01-11 21:22:05 +0700

    merapihkan halaman UI > main asdos

--- app/src/main/java/com/example/testingskripsinew/LoginUser.java
+++ app/src/main/java/com/example/testingskripsinew/LoginUser.java
@@ -2,7 +2,9 @@ package com.example.testingskripsinew;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginUser extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class LoginUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_user);
+    }
+
+    public void btnLogin(View view) {
+        Intent i = new Intent(this, MainActivityUser.class);
+        this.startActivity(i);
     }
 }
--- app/src/main/java/com/example/testingskripsinew/MainActivityAsdos.java
+++ app/src/main/java/com/example/testingskripsinew/MainActivityAsdos.java
@@ -2,7 +2,10 @@ package com.example.testingskripsinew;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivityAsdos extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivityAsdos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_asdos);
+    }
+
+    public void btnHistoryMhs(View view) {
+        Intent i = new Intent(this, MainActivityUser.class);
+        this.startActivity(i);
+    }
+
+    public void btnProfilAsdos(View view) {
+        Intent i = new Intent(this, MainActivityUser.class);
+        this.startActivity(i);
     }
 }
